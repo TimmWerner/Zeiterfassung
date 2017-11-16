861d04b852cc68a42e112b3b4b47892391e034c0
Jonas <jonas.schade3@gmail.com>
2017-11-16 18:07:22 +0100

    Webserver Docker-Container initial implementation

---
+++ webserver/server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+app.use(express.static('src/app'));
+app.use('/img', express.static('src/img'));
+
+app.listen(PORT, HOST);
